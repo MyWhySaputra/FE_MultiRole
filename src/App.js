be2ecdc9898208aaa782efd36faa45f582549247
MyWahyuSap <wahyusaputra222000@gmail.com>
2024-02-12 21:07:50 +0700

    12/02/2024

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./components/Login";
+import Users from "./pages/Users";
+import Products from "./pages/Products";
+import AddUser from "./pages/AddUser";
+import EditUser from "./pages/EditUser";
+import AddProduct from "./pages/AddProduct";
+import EditProduct from "./pages/EditProduct";
+
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/users/add" element={<AddUser />} />
+          <Route path="/users/edit/:id" element={<EditUser />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/add" element={<AddProduct />} />
+          <Route path="/products/edit/:id" element={<EditProduct />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }

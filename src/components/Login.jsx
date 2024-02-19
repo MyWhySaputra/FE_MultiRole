0cc360b86a862a8c44af6a75da38da7a20e3c109
MyWahyuSap <wahyusaputra222000@gmail.com>
2024-02-19 19:58:33 +0700

    19/02/2024

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -25,61 +25,55 @@ const Login = () => {
   };
 
   return (
-    <section className="hero h-screen w-full">
-      <div className="hero-body">
-        <div className="container">
-          <div className="grid place-items-center">
-            <div className="w-full max-w-xs">
-              <form
-                onSubmit={Auth}
-                className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-              >
-                {isError && <p className="text-center mb-4">{message}</p>}
-                <h1 className="text-2xl font-bold mb-6 text-center">Sign In</h1>
-                <div className="mb-4">
-                  <label
-                    className="block text-gray-700 text-sm font-bold mb-2"
-                    for="email"
-                  >
-                    Email
-                  </label>
-                  <input
-                    type="text"
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    placeholder="Email"
-                  />
-                </div>
-                <div className="mb-6">
-                  <label
-                    className="block text-gray-700 text-sm font-bold mb-2"
-                    for="password"
-                  >
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    placeholder="******"
-                  />
-                </div>
-                <div className="flex items-center justify-center">
-                  <button
-                    type="submit"
-                    className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
-                  >
-                    {isLoading ? "Loading..." : "Login"}
-                  </button>
-                </div>
-              </form>
-            </div>
+    <section className="h-screen w-full flex justify-center items-center">
+      <div className="w-full max-w-xs">
+        <form
+          onSubmit={Auth}
+          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        >
+          {isError && <p className="text-center mb-4">{message}</p>}
+          <h1 className="text-2xl font-bold mb-6 text-center">Sign In</h1>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              for="email"
+            >
+              Email
+            </label>
+            <input
+              type="text"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+            />
           </div>
-        </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              for="password"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="******"
+            />
+          </div>
+          <div className="flex items-center justify-center">
+            <button
+              type="submit"
+              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
+            >
+              {isLoading ? "Loading..." : "Login"}
+            </button>
+          </div>
+        </form>
       </div>
     </section>
   );
